c0b6a60d4ec2d74214ad04cc59f83d9b58eb46dc
sngsp <132871073+sngsp@users.noreply.github.com>
2024-03-12 10:12:55 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.14 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func solution(_ left:Int, _ right:Int) -> Int {
+    
+    var result = 0
+    
+    for i in left...right {
+        var count = 0
+        for j in 1...i {
+            if i % j == 0 {
+                count += 1
+            }
+        }
+        
+        if count % 2 == 0 {
+            result += i
+        } else {
+            result -= i
+        }
+    }
+    return result
+}
